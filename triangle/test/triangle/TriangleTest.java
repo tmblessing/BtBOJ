3f3317a65818ba126872c8f4fe4a88c3e585c461
Tom Blessing <tblessing@umass.edu>
2018-10-23 10:57:59 -0400

    First half of condition testing
    Up to lines 39

--- triangle/test/triangle/TriangleTest.java
+++ triangle/test/triangle/TriangleTest.java
@@ -23,6 +23,43 @@ public class TriangleTest {
     }
     
     public void test_condition() {
+        Type expected;
+        Type actual;
+        
+        //Tests 'false' for all conditions in line 20
+        expected = INVALID;
+        actual = Triangle.classify(0,0,0);
+        assertEquals(actual, expected);
+        
+        //Tests 'true' for lines 24, 27, 30
+        //Tests 'false' for line 33
+        //Tests 'true' for line 40
+        expected = EQUILATERAL;
+        actual = Triangle.classify(1,1,1);
+        assertEquals(actual, expected);
+        
+        //Test true and false for all conditions in line 34. 
+        //Tests 'true' for line 20 conditions
+        //Tests 'false' for lines 24, 27, 30, 33
+        expected = INVALID;
+        actual = Triangle.classify(1, 2, 3);
+        assertEquals(actual, expected);
+        
+        expected = INVALID;
+        actual = Triangle.classify(2, 3, 1);
+        assertEquals(actual, expected);
+        
+        expected = INVALID;
+        actual = Triangle.classify(3, 1 ,2);
+        assertEquals(actual, expected);
+        
+        //train = 1
+        
+        
+        //train = 2
+        
+        
+        //train = 3
     	
     }
     
