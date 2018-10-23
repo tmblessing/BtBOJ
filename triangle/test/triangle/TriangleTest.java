e5a796ffb6c1f287755c37044b473cd698e23bd7
Terrance Peters <terrancejpeters@gmail.com>
2018-10-23 10:58:01 -0400

    Triangle Condition Tests continue

--- triangle/test/triangle/TriangleTest.java
+++ triangle/test/triangle/TriangleTest.java
@@ -18,7 +18,50 @@ public class TriangleTest {
         assertEquals(actual, expected);
     }
     
+    @Test
     public void test_statement() {
+    	
+    	
+    	//Test an invalid triangle condition
+    	Type actual = Triangle.classify(0,0,0);
+    	Type expected = INVALID;
+    	assertEquals(actual, expected);
+    	
+    	actual = Triangle.classify(1,1,1);
+    	
+    	expected = EQUILATERAL;
+    	
+    	assertEquals(actual,expected);
+    	
+    }
+    
+    @Test
+    public void test_cond_invalid(){
+    	int a = 0;
+    	int b = 0;
+    	int c = 0; 
+    	
+    	//Test a non-existent triangle
+    	Type actual = Triangle.classify(a,b,c);
+    	Type expected = INVALID;
+    	assertEquals(actual, expected);
+    	
+    	a = 10;
+    	b = 10;
+    	
+    	//test incase of impossible Triangle dimensions
+    	actual = Triangle.classify(a,b,c);
+    	assertEquals(actual,expected);
+    	
+    }
+    
+    @Test
+    public void test_cond_scalene() {
+    	Type actual = Triangle.classify(3,5,6);
+    	Type expected = SCALENE;
+    	
+    	assertEquals(actual,expected);
+    	
     	
     }
     
